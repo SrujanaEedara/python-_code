e74bcbc602ccd74b5f80f03ddc0c3b1df2ee897f
SrujanaEedara <167171390+SrujanaEedara@users.noreply.github.com>
2024-05-03 22:14:22 +0530

    Create hacker rank task 8.py

---
+++ hacker rank task 8.py
@@ -0,0 +1,11 @@
+#In Python, a string can be split on a delimiter.
+
+def split_and_join(line):
+    a=line.split(" ")
+    b="-".join(a)
+    return(b)
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
